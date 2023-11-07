943615be05c9198d7adc7df05941c40a5d17cc7b
jla510 <jla510@student.ubc.ca>
2023-11-07 11:22:44 -0800

    testing crossovers for more pairs

--- ma_sim.py
+++ ma_sim.py
@@ -58,31 +58,48 @@ def process_results(results):
     results_list = [r.result_ob() for r in results]
     final_df = pd.DataFrame.from_dict(results_list)
 
-    print(final_df.info())
-    print(final_df.head())
+    final_df.to_pickle('ma_test_res.pkl')
+    print(final_df.shape, final_df.num_trades.sum())
+
+
+
+def get_test_pairs(pair_str):
+    existing_pairs = instrument.Instrument.get_instruments_dict().keys()
+    pairs = pair_str.split(",")
+    
+    test_list = []
+    for p1 in pairs:
+        for p2 in pairs:
+            p = f"{p1}_{p2}"
+            if p in existing_pairs:
+                test_list.append(p)
+    
+    print(test_list)
+    return test_list
 
 
 def run():
-    pairname = "GBP_JPY"
+    currencies = "GBP,EUR,USD,CAD,JPY,NZD,CHF"
     granularity = "H1"
-    ma_short = [8, 16, 32, 64]
-    ma_long = [32, 64, 96, 128, 256]
-    i_pair = instrument.Instrument.get_instruments_dict()[pairname]
-
-    price_data = get_price_data(pairname, granularity)
-    price_data = process_data(ma_short, ma_long, price_data)
+    ma_short = [4, 8, 16, 24, 32, 64]
+    ma_long = [8, 16, 32, 64, 96, 128, 256]
+    test_pairs = get_test_pairs(currencies)    
 
     results = []
-    
-    for _malong in ma_long:
-        for _mashort in ma_short:
-            if _mashort >= _malong:
-                continue
-            results.append(evaluate_pair(i_pair, _mashort, _malong, price_data.copy()))
+    for pairname in test_pairs:
+        print("running..", pairname)
+        i_pair = instrument.Instrument.get_instruments_dict()[pairname]
+
+        price_data = get_price_data(pairname, granularity)
+        price_data = process_data(ma_short, ma_long, price_data)
+
+        for _malong in ma_long:
+            for _mashort in ma_short:
+                if _mashort >= _malong:
+                    continue
+                results.append(evaluate_pair(i_pair, _mashort, _malong, price_data.copy()))
 
     process_results(results)
-
-
-
+    
 if __name__ == "__main__":
     run()
