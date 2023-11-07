67599761a3e6604a067b1ff988b1c3bae8e76894
jla510 <jla510@student.ubc.ca>
2023-11-06 23:32:24 -0800

    update instruments file

--- instrument.py
+++ instrument.py
@@ -12,7 +12,7 @@ class Instrument():
     def __repr__(self):
         return str(vars(self))
 
-    @classmethod
+    @classmethod # gets dataframe of the instrument
     def get_instruments_df(cls):
         return pd.read_pickle(utils.get_instruments_data_filename())
 
@@ -20,6 +20,37 @@ class Instrument():
     def get_instruments_list(cls):
         df = cls.get_instruments_df()
         return [Instrument(x) for x in df.to_dict(orient='records')]
+
+
+    '''
+    dict to access instruments by pair name
+
+    instrument_dict =
+    {
+        "EUR_USD" : Instrument(),
+        "EUR_GBP" : Instrument(),
+        "..." : Instrument(),
+    }
+
+    our_instrument = instrument_dict["EUR_USD"]
+    '''
+    @classmethod
+    def get_instruments_dict(cls):
+        i_list = cls.get_instruments_list()
+        i_keys = [x.name for x in i_list]
+        return { k:v for (k,v) in zip(i_keys, i_list) }
+
+    @classmethod
+    def get_instrument_by_name(cls, pairname):
+        d = cls.get_instruments_dict()
+        if pairname in d:
+            return d[pairname]
+        else:
+            return None
+
+
+
+
 
 
 if __name__ == "__main__":
